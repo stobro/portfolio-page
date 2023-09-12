dacbec43b1006629523de82c62b013fb9231b0d7
stobro <144771827+stobro@users.noreply.github.com>
2023-09-12 11:49:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showProjectDetails(projectId) {
+    alert("Details für " + projectId);
+    // Hier können Sie eine Modal-Box oder eine separate Seite mit Projektinformationen anzeigen.
+}
